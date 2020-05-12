870e6becc24a826d353d30d378ae4de474d13dc1
rpi <haha@haha.haha>
2020-05-12 09:40:40 -0400

    includes addtional commands

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+#rfcomm release 0
+sudo rfcomm bind /dev/rfcomm0 00:1D:A5:05:AF:10
+./fix_touchscreen
+
 python3 server.py > server.log 2>&1 &
 echo "Server IP:   "`hostname -I`
 echo "Server port: 6626"
