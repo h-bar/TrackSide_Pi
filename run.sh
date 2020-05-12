191dbfcc885bbcaf2e497883cd60752ad5ada758
rpi <haha@haha.haha>
2020-05-12 09:45:48 -0400

    temp disable obd

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #rfcomm release 0
-sudo rfcomm bind /dev/rfcomm0 00:1D:A5:05:AF:10
+# sudo rfcomm bind /dev/rfcomm0 00:1D:A5:05:AF:10
 ./fix_touchscreen
 
 python3 server.py > server.log 2>&1 &
 echo "Server IP:   "`hostname -I`
 echo "Server port: 6626"
-python3 app.py > app.log 2>&1 &
+sudo python3 app.py > app.log 2>&1 &
